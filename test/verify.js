4df255e6df71a87968e43648b31567024f67730a
Matt Bell <mappum@gmail.com>
2018-01-29 19:15:43 -0800

    Added verification tests

---
+++ test/verify.js
@@ -0,0 +1,199 @@
+let test = require('ava')
+let { mockDb } = require('./common.js')
+let merk = require('../src/merk.js')
+
+test('verify single-node root proof', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+  state.foo = 'bar'
+  await merk.commit(state)
+
+  let rootHash = merk.hash(state)
+  let proof = await merk.proof(state)
+  let value = merk.verify(rootHash, proof)
+
+  t.deepEqual(value, { foo: 'bar' })
+})
+
+test('verify single-node non-object child proof', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+  state.foo = 'bar'
+  await merk.commit(state)
+
+  let rootHash = merk.hash(state)
+  let proof = await merk.proof(state, 'foo')
+  let value = merk.verify(rootHash, proof, 'foo')
+
+  t.is(value, 'bar')
+})
+
+test('verify multi-node root proof', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+  state.foo = 'bar'
+  state.baz = { x: 123 }
+  await merk.commit(state)
+
+  let rootHash = merk.hash(state)
+  let proof = await merk.proof(state)
+  let value = merk.verify(rootHash, proof)
+
+  t.deepEqual(value, { foo: 'bar', baz: { x: 123 } })
+})
+
+test('verify multi-node child proof', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+  state.foo = 'bar'
+  state.baz = { x: 123 }
+  await merk.commit(state)
+
+  let rootHash = merk.hash(state)
+  let proof = await merk.proof(state, 'baz')
+  let value = merk.verify(rootHash, proof, 'baz')
+
+  t.deepEqual(value, { x: 123 })
+})
+
+test('verify multi-node non-object child proof', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+  state.foo = 'bar'
+  state.baz = { x: 123 }
+  await merk.commit(state)
+
+  let rootHash = merk.hash(state)
+  let proof = await merk.proof(state, 'baz.x')
+  let value = merk.verify(rootHash, proof, 'baz.x')
+
+  t.is(value, 123)
+})
+
+test('verify with incorrect root hash', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+
+  state.foo = 'bar'
+  await merk.commit(state)
+  let rootHash = merk.hash(state)
+
+  state.baz = { x: 123 }
+  await merk.commit(state)
+
+  let proof = await merk.proof(state, 'baz')
+  try {
+    merk.verify(rootHash, proof, 'baz')
+    t.fail()
+  } catch (err) {
+    t.is(err.message, 'Proof does not match expected root hash')
+  }
+})
+
+test('verify with array range', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+
+  // state.abc = {n: 123}
+  // state.array = [{n: 0}, {n: 1}, {n: 2}, {n: 3}]
+  // state.xyz = {n: 456}
+  // await merk.commit(state)
+  // let rootHash = merk.hash(state)
+  // let proof = await merk.proof(state, 'array')
+
+  let rootHash = 'ac2323c6ebaafb3174c6dfac855ed0652f4651af'
+  let proof = {
+    left: {
+      left: { left: null, right: null, key: '.abc', value: '{"n":123}' },
+      right: { left: null, right: null, key: '.array.0', value: '{"n":0}' },
+      key: '.array',
+      value: '[]'
+    },
+    right: {
+      left: { left: null, right: null, key: '.array.2', value: '{"n":2}' },
+      right: { left: null, right: null, key: '.xyz', value: '{"n":456}' },
+      key: '.array.3',
+      value: '{"n":3}'
+    },
+    key: '.array.1',
+    value: '{"n":1}'
+  }
+
+  let value = merk.verify(rootHash, proof, 'array')
+  t.deepEqual(value, [{n: 0}, {n: 1}, {n: 2}, {n: 3}])
+})
+
+test('verify with array child', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+
+  state.abc = {n: 123}
+  state.array = [{n: 0}, {n: 1}, {n: 2}, {n: 3}]
+  state.xyz = {n: 456}
+  await merk.commit(state)
+  let rootHash = merk.hash(state)
+  let proof = await merk.proof(state, 'array.1')
+
+  let value = merk.verify(rootHash, proof, 'array.1')
+  t.deepEqual(value, {n: 1})
+})
+
+test('verify with unproven range', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+
+  let rootHash = 'ac2323c6ebaafb3174c6dfac855ed0652f4651af'
+  let proof = {
+    left: {
+      left: 'xxMB1E9t2x1ZoUbKiEi+ffyH7sI=',
+      right: { left: null, right: null, key: '.array.0', value: '{"n":0}' },
+      key: '.array',
+      value: '[]'
+    },
+    right: {
+      left: { left: null, right: null, key: '.array.2', value: '{"n":2}' },
+      right: { left: null, right: null, key: '.xyz', value: '{"n":456}' },
+      key: '.array.3',
+      value: '{"n":3}'
+    },
+    key: '.array.1',
+    value: '{"n":1}'
+  }
+
+  try {
+    merk.verify(rootHash, proof, 'array')
+    t.fail()
+  } catch (err) {
+    t.is(err.message, 'First key greater than beginning of range')
+  }
+})
+
+test('verify with unproven range', async (t) => {
+  let db = mockDb()
+  let state = await merk(db)
+
+  let rootHash = 'ac2323c6ebaafb3174c6dfac855ed0652f4651af'
+  let proof = {
+    left: {
+      left: { left: null, right: null, key: '.abc', value: '{"n":123}' },
+      right: { left: null, right: null, key: '.array.0', value: '{"n":0}' },
+      key: '.array',
+      value: '[]'
+    },
+    right: {
+      left: { left: null, right: null, key: '.array.2', value: '{"n":2}' },
+      right: 'dI02/HM0MSGWx6UTqX0zG3TCE1Q=',
+      key: '.array.3',
+      value: '{"n":3}'
+    },
+    key: '.array.1',
+    value: '{"n":1}'
+  }
+
+  try {
+    merk.verify(rootHash, proof, 'array')
+    t.fail()
+  } catch (err) {
+    t.is(err.message, 'Last key less than end of range')
+  }
+})
