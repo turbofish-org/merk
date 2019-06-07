cce62f2651fc200996d87b81b8bc501985736cea
Matt Bell <mappum@gmail.com>
2019-06-06 22:01:13 -0700

    Add merk delete batch test case

--- tests/merk.rs
+++ tests/merk.rs
@@ -55,3 +55,29 @@ fn merk_proof() {
 
     merk.destroy().unwrap();
 }
+
+#[test]
+fn merk_delete_1k() {
+    let mut merk = Merk::open("./test_merk_delete_1k.db").unwrap();
+
+    let mut keys: Vec<[u8; 4]> = Vec::with_capacity(1001);
+    let mut batch: Vec<TreeBatchEntry> = Vec::with_capacity(1001);
+    for i in 0..=1000 {
+        keys.push((i as u32).to_be_bytes());
+    }
+    for i in 0..=1000 {
+        batch.push((&keys[i], TreeOp::Put(b"xyz")));
+    }
+    merk.apply(&mut batch).unwrap();
+
+    batch.clear();
+    for i in 0..1000 {
+        batch.push((&keys[i], TreeOp::Delete));
+    }
+    merk.apply(&mut batch).unwrap();
+
+    assert_eq!(&merk.tree.as_ref().unwrap().key, &keys[1000]);
+    assert_eq!(&merk.tree.as_ref().unwrap().height(), &(1 as u8));
+
+    merk.destroy().unwrap();
+}
