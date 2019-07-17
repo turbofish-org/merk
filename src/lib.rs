cb44864b469ea3ad3e256e76784b3ee009db420c
Matt Bell <mappum@gmail.com>
2019-07-16 17:34:20 -0700

    Cleared out existing code so we can start fresh

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,5 @@
-#![feature(trait_alias)]
-
 #[macro_use]
 extern crate error_chain;
-#[macro_use]
-extern crate serde_derive;
-extern crate bincode;
-extern crate blake2_rfc;
-extern crate colored;
-extern crate hex;
-extern crate num_cpus;
-extern crate rocksdb;
-extern crate serde;
 
-mod test_util;
-mod ops;
-mod proof;
-mod tree;
 mod error;
-mod merk;
-
-// collect all internal module exports and re-export as root module
-pub use crate::error::*;
-pub use crate::merk::*;
-pub use crate::tree::*;
-pub use crate::proof::*;
+mod tree;
--- src/ops/context.rs
+++
@@ -1,9 +0,0 @@
-use super::{Batch, Op, Worker};
-use crate::tree::{Tree, Node};
-use crate::error::Result;
-
-pub struct Context<'a> {
-    pub batch: &'a Batch<'a>,
-    pub workers: &'a [Worker],
-    pub get_node: Box<dyn Fn(Vec<u8>) -> Result<Node>>
-}
