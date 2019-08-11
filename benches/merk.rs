c44efe50a891162d3929872c5f6facc3c060f416
Matt Bell <mappum@gmail.com>
2019-08-11 10:27:30 -0700

    Added Merk random insert bench

--- benches/merk.rs
+++ benches/merk.rs
@@ -29,47 +29,23 @@ fn insert_1m_10k_seq_rocksdb_noprune(b: &mut Bencher) {
     });
 }
 
-// #[bench]
-// fn insert_1m_10k_rand_memonly(b: &mut Bencher) {
-//     let initial_size = 1_000_000;
-//     let batch_size = 10_000;
+#[bench]
+fn insert_1m_10k_rand_rocksdb_noprune(b: &mut Bencher) {
+    let initial_size = 1_000_000;
+    let batch_size = 10_000;
 
-//     let mut tree = Owner::new(make_tree_rand(initial_size, batch_size, 0));
+    let path = thread::current().name().unwrap().to_owned();
+    let mut merk = TempMerk::open(path).expect("failed to open merk");
 
-//     let mut i = initial_size / batch_size;
-//     b.iter(|| {
-//         let batch = make_batch_rand(batch_size, i);
-//         tree.own(|tree| (apply_memonly_unchecked(tree, &batch), 0));
-//         i += 1;
-//     });
-// }
+    for i in 0..(initial_size / batch_size) {
+        let batch = make_batch_rand(batch_size, i);
+        merk.apply_unchecked(&batch).expect("apply failed");
+    }
 
-// #[bench]
-// fn update_1m_10k_seq_memonly(b: &mut Bencher) {
-//     let initial_size = 1_000_000;
-//     let batch_size = 10_000;
-
-//     let mut tree = Owner::new(make_tree_seq(initial_size));
-
-//     let mut i = 0;
-//     b.iter(|| {
-//         let batch = make_batch_seq((i * batch_size)..((i+1) * batch_size));
-//         tree.own(|tree| (apply_memonly_unchecked(tree, &batch), 0));
-//         i = (i + 1) % (initial_size / batch_size);
-//     });
-// }
-
-// #[bench]
-// fn update_1m_10k_rand_memonly(b: &mut Bencher) {
-//     let initial_size = 1_000_000;
-//     let batch_size = 10_000;
-
-//     let mut tree = Owner::new(make_tree_rand(initial_size, batch_size, 0));
-
-//     let mut i = 0;
-//     b.iter(|| {
-//         let batch = make_batch_rand(batch_size, i);
-//         tree.own(|tree| (apply_memonly_unchecked(tree, &batch), 0));
-//         i = (i + 1) % (initial_size / batch_size);
-//     });
-// }
+    let mut i = initial_size / batch_size;
+    b.iter(|| {
+        let batch = make_batch_rand(batch_size, i);
+        merk.apply_unchecked(&batch).expect("apply failed");
+        i += 1;
+    });
+}
