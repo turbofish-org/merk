f5555b1766d47882e48ed2d626da55bc0c6d4d7d
Matt Bell <mappum@gmail.com>
2019-08-19 18:49:27 -0700

    Simplify iterator code

--- src/tree/iter.rs
+++ src/tree/iter.rs
@@ -6,6 +6,17 @@ struct StackItem<'a> {
 }
 
 impl<'a> StackItem<'a> {
+    fn new(tree: &'a Tree) -> Self {
+        StackItem {
+            tree,
+            traversed: (
+                tree.child(true).is_none(),
+                false,
+                tree.child(false).is_none()
+            )
+        }
+    }
+
     fn to_entry(&self) -> (Vec<u8>, Vec<u8>) {
         (
             self.tree.key().to_vec(),
@@ -18,12 +29,16 @@ pub struct Iter<'a> {
     stack: Vec<StackItem<'a>>
 }
 
-impl Tree {
-    pub fn iter<'a>(&'a self) -> Iter<'a> {
-        let stack = vec![
-            StackItem { tree: self, traversed: (false, false, false) }
-        ];
+impl<'a> Iter<'a> {
+    pub fn new(tree: &'a Tree) -> Self {
+        let stack = vec![ StackItem::new(tree) ];
         Iter { stack }
+    }
+}
+
+impl<'a> Tree {
+    pub fn iter(&'a self) -> Iter<'a> {
+        Iter::new(self)
     }
 }
 
@@ -31,37 +46,24 @@ impl<'a> Iterator for Iter<'a> {
     type Item = (Vec<u8>, Vec<u8>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        println!("next {} {:?}", self.stack.len(), self.stack.last().map(|l| l.tree.key()));
-
         if self.stack.is_empty() {
             return None;
         }
 
-        let last = self.stack.last().unwrap();
+        let last = self.stack.last_mut().unwrap();
         if !last.traversed.0 {
-            if let Some(tree) = last.tree.child(true) {
-                self.stack.last_mut().unwrap().traversed.0 = true;
-                self.stack.push(StackItem { tree, traversed: (false, false, false) });
-                self.next()
-            } else {
-                let last = self.stack.last_mut().unwrap();
-                last.traversed.0 = true;
-                self.next()
-            }
+            last.traversed.0 = true;
+            let tree = last.tree.child(true).unwrap();
+            self.stack.push(StackItem::new(tree));
+            self.next()
         } else if !last.traversed.1 {
-            let last = self.stack.last_mut().unwrap();
             last.traversed.1 = true;
             Some(last.to_entry())
         } else if !last.traversed.2 {
-            if let Some(tree) = last.tree.child(false) {
-                let last = self.stack.last_mut().unwrap();
-                last.traversed.2 = true;
-                self.stack.push(StackItem { tree, traversed: (false, false, false) });
-                self.next()
-            } else {
-                self.stack.pop();
-                self.next()
-            }
+            last.traversed.2 = true;
+            let tree = last.tree.child(false).unwrap();
+            self.stack.push(StackItem::new(tree));
+            self.next()
         } else {
             self.stack.pop();
             self.next()
