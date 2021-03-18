f9c36da30f4a4d28d81fac7fe319384bf630ca3c
Judd Keppel <kepzorz@gmail.com>
2021-03-18 13:54:15 -0500

    Added LayerIter to iterate over a given layer of proofs::Tree nodes
    Co-authored-by: Matt Bell <mappum@gmail.com>

--- src/proofs/verify.rs
+++ src/proofs/verify.rs
@@ -2,15 +2,16 @@ use super::{Decoder, Node, Op};
 use crate::error::Result;
 use crate::tree::{kv_hash, node_hash, Hash, NULL_HASH};
 use failure::bail;
-struct Child {
-    tree: Box<Tree>,
+
+pub(crate) struct Child {
+    pub(crate) tree: Box<Tree>,
     hash: Hash,
 }
 
 /// A binary tree data structure used to represent a select subset of a tree
 /// when verifying Merkle proofs.
-struct Tree {
-    node: Node,
+pub(crate) struct Tree {
+    pub(crate) node: Node,
     left: Option<Child>,
     right: Option<Child>,
 }
@@ -25,9 +26,15 @@ impl From<Node> for Tree {
     }
 }
 
+impl PartialEq for Tree {
+    fn eq(&self, other: &Self) -> bool {
+        self.hash() == other.hash()
+    }
+}
+
 impl Tree {
     /// Returns an immutable reference to the child on the given side, if any.
-    fn child(&self, left: bool) -> Option<&Child> {
+    pub(crate) fn child(&self, left: bool) -> Option<&Child> {
         if left {
             self.left.as_ref()
         } else {
@@ -63,7 +70,7 @@ impl Tree {
     /// (zero-filled).
     #[inline]
     fn child_hash(&self, left: bool) -> Hash {
-        self.child(left).map_or(NULL_HASH, |c| c.tree.hash())
+        self.child(left).map_or(NULL_HASH, |c| c.hash)
     }
 
     /// Consumes the tree node, calculates its hash, and returns a `Node::Hash`
@@ -73,7 +80,7 @@ impl Tree {
     }
 
     /// Gets or computes the hash for this tree node.
-    fn hash(&self) -> Hash {
+    pub(crate) fn hash(&self) -> Hash {
         fn compute_hash(tree: &Tree, kv_hash: Hash) -> Hash {
             node_hash(&kv_hash, &tree.child_hash(true), &tree.child_hash(false))
         }
@@ -87,9 +94,75 @@ impl Tree {
             }
         }
     }
+
+    pub(crate) fn layer(&self, depth: usize) -> LayerIter {
+        LayerIter::new(self, depth)
+    }
 }
 
-fn execute<I, F>(ops: I, collapse: bool, mut visit_node: F) -> Result<Tree>
+pub(crate) struct LayerIter<'a> {
+    stack: Vec<&'a Tree>,
+    depth: usize,
+}
+
+impl<'a> LayerIter<'a> {
+    fn new(tree: &'a Tree, depth: usize) -> Self {
+        let mut iter = LayerIter {
+            stack: Vec::with_capacity(depth),
+            depth,
+        };
+
+        iter.traverse_to_start(tree, depth);
+        iter
+    }
+
+    fn traverse_to_start(&mut self, tree: &'a Tree, remaining_depth: usize) {
+        self.stack.push(tree);
+
+        if remaining_depth == 0 {
+            return;
+        }
+
+        if let Some(child) = tree.child(true) {
+            self.traverse_to_start(&child.tree, remaining_depth - 1)
+        }
+    }
+}
+
+impl<'a> Iterator for LayerIter<'a> {
+    type Item = &'a Tree;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.stack.pop();
+        let mut popped = item;
+
+        loop {
+            if self.stack.is_empty() {
+                return item;
+            }
+
+            let parent = self.stack.last().unwrap();
+            let left_child = parent.child(true).unwrap();
+            let right_child = parent.child(false).unwrap();
+
+            if left_child.tree.as_ref() == popped.unwrap() {
+                self.stack.push(&right_child.tree);
+
+                while self.stack.len() - 1 < self.depth {
+                    let parent = self.stack.last().unwrap();
+                    let left_child = parent.child(true).unwrap();
+                    self.stack.push(&left_child.tree);
+                }
+
+                return item;
+            } else {
+                popped = self.stack.pop();
+            }
+        }
+    }
+}
+
+pub(crate) fn execute<I, F>(ops: I, collapse: bool, mut visit_node: F) -> Result<Tree>
 where
     I: IntoIterator<Item = Result<Op>>,
     F: FnMut(&Node) -> Result<()>,
@@ -207,12 +280,11 @@ pub fn verify_query(
         debug_assert_eq!(keys.len(), output.len());
     }
 
-    let hash = root.into_hash().hash();
-    if hash != expected_hash {
+    if root.hash() != expected_hash {
         bail!(
             "Proof did not match expected hash\n\tExpected: {:?}\n\tActual: {:?}",
             expected_hash,
-            hash
+            root.hash()
         );
     }
 
@@ -298,5 +370,40 @@ mod test {
     #[test]
     fn absent_and_present_verify() {
         verify_test(vec![vec![5], vec![6]], vec![Some(vec![5]), None]);
+    }
+
+    #[test]
+    fn layer_iter() {
+        let make_node = |i| -> super::Tree { Node::KV(vec![i], vec![]).into() };
+        let assert_node = |node: &super::Tree, i| match node.node {
+            Node::KV(ref key, _) => assert_eq!(key[0], i),
+            _ => unreachable!(),
+        };
+
+        let mut tree = make_node(3);
+        let mut left = make_node(1);
+        left.attach(true, make_node(0)).unwrap();
+        left.attach(false, make_node(2)).unwrap();
+        let mut right = make_node(5);
+        right.attach(true, make_node(4)).unwrap();
+        right.attach(false, make_node(6)).unwrap();
+        tree.attach(true, left).unwrap();
+        tree.attach(false, right).unwrap();
+
+        let mut iter = tree.layer(0);
+        assert_node(iter.next().unwrap(), 3);
+        assert!(iter.next().is_none());
+
+        let mut iter = tree.layer(1);
+        assert_node(iter.next().unwrap(), 1);
+        assert_node(iter.next().unwrap(), 5);
+        assert!(iter.next().is_none());
+
+        let mut iter = tree.layer(2);
+        assert_node(iter.next().unwrap(), 0);
+        assert_node(iter.next().unwrap(), 2);
+        assert_node(iter.next().unwrap(), 4);
+        assert_node(iter.next().unwrap(), 6);
+        assert!(iter.next().is_none());
     }
 }
