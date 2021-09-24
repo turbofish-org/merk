91ac7775be4eb146b4483e7b8b466ace9da8e571
Charlie Little <cwlittle@utexas.edu>
2021-09-24 15:09:13 -0500

    Add ProofHashMismatch Error variant

--- src/error.rs
+++ src/error.rs
@@ -18,6 +18,8 @@ pub enum Error {
     PathError(String),
     #[error("Chunk Processing Error: {}")]
     ChunkProcessingError(String),
+    #[error("Proof did not match expected hash\n\tExpected: {0}\n\tActual: {1}")]
+    ProofHashMismatch(String, String),
     #[error("Unknown Error")]
     Unknown,
 }
