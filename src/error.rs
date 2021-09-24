e880912a54d5f930f2208a964dd0ae484f52eb7f
Charlie Little <cwlittle@utexas.edu>
2021-09-24 14:30:56 -0500

    Add Error variants for Fetches and IOoB

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,10 @@ pub use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
+    #[error("Index OoB Error: {0}")]
+    IndexOutOfBounds(String),
+    #[error("Fetch Error: {0}")]
+    FetchError(String),
     #[error("Unknown Error")]
     Unknown,
 }
