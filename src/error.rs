899626330292dd47894685e0c637c73fc161111e
Charlie Little <cwlittle@utexas.edu>
2021-09-24 14:05:07 -0500

    Add primitive MyError type

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,10 @@
 pub use failure::Error;
+pub use thiserror::Error;
+
+#[derive(thiserror::Error, Debug)]
+pub enum MyError {
+    #[error("Unknown Error")]
+    Unknown,
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
