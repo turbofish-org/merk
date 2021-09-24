1c5422faa5f137b0847459e7d533caf2c8c8ca3a
Charlie Little <cwlittle@utexas.edu>
2021-09-24 17:45:09 -0500

    Remove transparent error conversion from failure::error

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,6 @@ pub use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error(transparent)]
-    FailureError(#[from] failure::Error),
     #[error("Index OoB Error: {0}")]
     IndexOutOfBounds(String),
     #[error("Fetch Error: {0}")]
