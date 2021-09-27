d04b9d3b42a9c6a7dd49e59950c2cc2dca838bd8
Charlie Little <cwlittle@utexas.edu>
2021-09-27 14:20:20 -0500

    Alphabetize error variants

--- src/error.rs
+++ src/error.rs
@@ -2,46 +2,46 @@ pub use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error(transparent)]
-    EdError(#[from] ed::Error),
-    #[error(transparent)]
-    RocksDBError(#[from] rocksdb::Error),
-    #[error("Index OoB Error: {0}")]
-    IndexOutOfBounds(String),
-    #[error("Fetch Error: {0}")]
-    FetchError(String),
-    #[error("Key not found: {0}")]
-    KeyNotFound(String),
+    #[error("Attach Error: {0}")]
+    AttachError(String),
     #[error("Batch Key Error: {0}")]
     BatchKey(String),
-    #[error("Key Error: {0}")]
-    KeyError(String),
-    #[error("Path Error: {0}")]
-    PathError(String),
-    #[error("Chunk Processing Error: {0}")]
-    ChunkProcessingError(String),
-    #[error("Proof did not match expected hash\n\tExpected: {0:?}\n\tActual: {1:?}")]
-    ProofHashMismatch([u8; 32], [u8; 32]),
-    #[error("Proof Error: {0}")]
-    ProofError(String),
-    #[error("Leaf Chunk proof did not match expected hash\n\tExpected: {0:?}\n\tActual: {1:?}")]
-    LeafChunkHashMismatch([u8; 32], [u8; 32]),
-    #[error("Query Error: {0}")]
-    QueryError(String),
     #[error("Bound Error: {0}")]
     BoundError(String),
+    #[error("Chunk Processing Error: {0}")]
+    ChunkProcessingError(String),
+    #[error(transparent)]
+    EdError(#[from] ed::Error),
+    #[error("Fetch Error: {0}")]
+    FetchError(String),
+    #[error("Index OoB Error: {0}")]
+    IndexOutOfBounds(String),
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error("Tried to delete non-existent key {0:?}")]
+    KeyDeleteError(Vec<u8>),
+    #[error("Key Error: {0}")]
+    KeyError(String),
+    #[error("Key not found: {0}")]
+    KeyNotFound(String),
+    #[error("Leaf Chunk proof did not match expected hash\n\tExpected: {0:?}\n\tActual: {1:?}")]
+    LeafChunkHashMismatch([u8; 32], [u8; 32]),
+    #[error("Path Error: {0}")]
+    PathError(String),
+    #[error("Proof Error: {0}")]
+    ProofError(String),
+    #[error("Proof did not match expected hash\n\tExpected: {0:?}\n\tActual: {1:?}")]
+    ProofHashMismatch([u8; 32], [u8; 32]),
+    #[error("Query Error: {0}")]
+    QueryError(String),
+    #[error(transparent)]
+    RocksDBError(#[from] rocksdb::Error),
+    #[error("Stack Underflow")]
+    StackUnderflow,
     #[error("Tree Error: {0}")]
     TreeError(String),
     #[error("Unexpected Node Error: {0}")]
     UnexpectedNodeError(String),
-    #[error("Attach Error: {0}")]
-    AttachError(String),
-    #[error("Tried to delete non-existent key {0:?}")]
-    KeyDeleteError(Vec<u8>),
-    #[error(transparent)]
-    IOError(#[from] std::io::Error),
-    #[error("Stack Underflow")]
-    StackUnderflow,
     #[error("Unknown Error")]
     Unknown,
 }
