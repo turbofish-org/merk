e33f092d9e1306d62001f83f52aaec1b3fb44d4b
Charlie Little <cwlittle@utexas.edu>
2024-12-18 11:48:08 -0600

    Add Debug to Tree

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -38,7 +38,7 @@ pub struct TreeInner {
 /// Trees' inner fields are stored on the heap so that nodes can recursively
 /// link to each other, and so we can detach nodes from their parents, then
 /// reattach without allocating or freeing heap memory.
-#[derive(Clone, PartialEq, Eq, Encode, Decode)]
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
 pub struct Tree {
     inner: Box<TreeInner>,
 }
